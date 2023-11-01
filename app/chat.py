0bdbd992d7636cc82cf288322df46d0715393970
henriquetg1 <henriquetg1@al.insper.edu.br>
2023-11-01 15:33:14 -0300

    tentativa de unir css com streamlit

--- app/chat.py
+++ app/chat.py
@@ -12,32 +12,28 @@ def get_messages_from_server():
         print(f"Error response: {response.status_code}, {response.text}")
         return []
 
-# Display the chat history or incoming messages
-# Fetch and display messages
+# Mostra as mensagens do servidor
 def display_messages():
     messages = get_messages_from_server()
 
-    # Reverse the order of messages so that new messages appear at the bottom
     messages = messages[::-1]
     messages.reverse()
 
-    # Create a text area to display messages
     message_display = st.text_area("Mensagens", value="\n".join(message['message'] for message in messages), height=300, key="messages_display", disabled=True)
 
-# Create an input box for the user to type messages
-message = st.text_input("Digite sua mensagem:")
+message = st.text_input("Digite sua mensagem:", key="message_input")
 
 def send_message_to_server(message):
     response = requests.post(f'{url_base}/send_message', data={'message': message})
     print(response.status_code)
     return response.status_code == 200
 
-# Create a button to send the message to the server
+# Botão para enviar a mensagem
 if st.button("Send"):
     if message:
         if send_message_to_server(message):
             st.success("Mensagem enviada com sucesso")
-            # Clear the input box after sending the message
+            # Limpa o campo de mensagem
             message = ""
         else:
             st.error("Falha ao enviar mensagem")
@@ -45,3 +41,8 @@ if st.button("Send"):
         st.warning("Por favor, digite sua mensagem")
 
 display_messages()
+
+# Tentativa de mudar o estilo do app
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
